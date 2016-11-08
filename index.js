b72831b90c21ea93077007b401eefe82e28be6d1
david90 <david.ng@oursky.com>
2016-11-08 18:45:58 +0800

    set up skygear

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-// You can require() this file in a CommonJS environment.
 require('./dist/js/flat-ui.js');
+
+
+// Include Skygear 
+skygear.config({
+  'endPoint': 'https://dinnerpoll.skygeario.com/', // trailing slash is required
+  'apiKey': 'ae9bb687d6b743b9803f96e8b3e4d970',
+}).then(() => {
+  console.log('skygear container is now ready for making API calls.');
+}, (error) => {
+  console.error(error);
+});
+
+// Login logic
+
+// Poll Logic
+
+// Show poll Logic
