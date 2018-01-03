acc59ff687d77d92c9ecdfd8ac991c227b22568b
Rick Mak <rick.mak@gmail.com>
2018-01-03 14:08:47 +0800

    Change to pubsub

--- autoreload.js
+++ autoreload.js
@@ -1,26 +1,26 @@
 var autoReloadChart;
 
- function startAutoReload() {
-  console.log("auto reload");
-  autoReloadChart = setInterval(function(){reloadChart()}, 3000);
-}
-
-function stopAutoReload() {
-  console.log("stoped auto reload");
-  clearTimeout(autoReloadChart);
-}
-
-//Using pubsub (comment line 3-11, uncomment the following lines to enable pubsub auto reload)
-
 // function startAutoReload() {
 //   console.log("auto reload");
-//   skygear.on('voted', (data) => {
-//     console.log(data)
-//     reloadChart()
-//   });
+//   autoReloadChart = setInterval(function(){reloadChart()}, 3000);
 // }
 
 // function stopAutoReload() {
 //   console.log("stoped auto reload");
-//   skygear.off('voted');
+//   clearTimeout(autoReloadChart);
 // }
+
+//Using pubsub (comment line 3-11, uncomment the following lines to enable pubsub auto reload)
+
+function startAutoReload() {
+  console.log("auto reload");
+  skygear.pubsub.on('voted', (data) => {
+    console.log(data)
+    reloadChart()
+  });
+}
+
+function stopAutoReload() {
+  console.log("stoped auto reload");
+  skygear.pubsub.off('voted');
+}
